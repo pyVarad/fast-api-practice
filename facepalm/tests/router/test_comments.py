94c5ea85eeea7dcd79a0f78d8a37afcdeab92011
varad <py.varad@gmail.com>
2024-08-18 16:43:50 +0530

    updated test cases for comments and posts

---
+++ facepalm/tests/router/test_comments.py
@@ -0,0 +1,25 @@
+
+
+import pytest
+from httpx import AsyncClient
+
+
+async def create_post(body:str, async_client:AsyncClient) -> dict:
+    response = await async_client.post("/posts/", json={"body": body})
+    return response.json()
+
+
+@pytest.fixture()
+async def created_post(async_client: AsyncClient):
+    return await create_post("Test_Post", async_client)
+
+@pytest.fixture()
+async def add_new_comment(async_client:AsyncClient, created_post: dict):
+    response = await async_client.post("/posts/0/comment", json={"body": "Sample comment"})
+    return response.json()
+
+
+@pytest.mark.anyio
+async def test_get_all_comments(async_client:AsyncClient, add_new_comment: dict):
+    response = await async_client.get("/posts/0")
+    assert response.status_code == 200
--- facepalm/tests/router/test_post.py
+++ facepalm/tests/router/test_post.py
@@ -3,7 +3,7 @@ from httpx import AsyncClient
 
 
 async def create_post(body:str, async_client:AsyncClient) -> dict:
-    response = async_client.post("/posts/", json={"body": body})
+    response = await async_client.post("/posts/", json={"body": body})
     return response.json()
 
 
@@ -18,3 +18,14 @@ async def test_simple_post(async_client: AsyncClient):
     response = await async_client.post("/posts/", json={"body": body})
     assert response.status_code == 201
     assert {"id": 0, "body": body}.items() <= response.json().items()
+
+@pytest.mark.anyio
+async def test_post_without_body(async_client: AsyncClient):
+    response = await async_client.post("/posts/", json={})
+    assert response.status_code == 422
+
+@pytest.mark.anyio
+async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
+    response = await async_client.get("/posts/")
+    assert response.status_code == 200
+    assert response.json() == [created_post]
