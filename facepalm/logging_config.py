ef73dc8a5c830dfdf4bc9a6023a0856e3361a86b
varad <py.varad@gmail.com>
2024-08-21 22:11:34 +0530

    logging_config first version

---
+++ facepalm/logging_config.py
@@ -0,0 +1,33 @@
+from logging.config import dictConfig
+
+from facepalm.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%y-%m-%dT%H:%M:%S",
+                    "format": "%(name)s:%(lineno)d - %(message)s",
+                }
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                }
+            },
+            "loggers": {
+                "facepalm": {
+                    "handlers": ["default"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                }
+            },
+        }
+    )
--- facepalm/main.py
+++ facepalm/main.py
@@ -3,11 +3,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from facepalm.database import database
+from facepalm.logging_config import configure_logging
 from facepalm.router.post import router as post_router
 
+# logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    # logger.info("Hello World")
     await database.connect()
     yield
     await database.disconnect()
