db6281397988efb05944637a2f5f991ca31afec7
varad <py.varad@gmail.com>
2024-08-22 06:21:18 +0530

    Added support for logging with correlation_id

--- facepalm/logging_config.py
+++ facepalm/logging_config.py
@@ -8,11 +8,24 @@ def configure_logging() -> None:
         {
             "version": 1,
             "disable_existing_loggers": False,
+            "filters": {
+                "correlation_id": {
+                    "()": "asgi_correlation_id.CorrelationIdFilter",
+                    "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                    "default_value": "-"
+                }
+            },
             "formatters": {
                 "console": {
                     "class": "logging.Formatter",
                     "datefmt": "%y-%m-%dT%H:%M:%S",
-                    "format": "%(name)s:%(lineno)d - %(message)s",
+                    "format": "(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | [%(correlation_id)s] %(name)s:%(lineno)d "
+                              "- %(message)s",
                 }
             },
             "handlers": {
@@ -20,11 +33,37 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
+                    "filters": ["correlation_id"]
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "facepalm.log",
+                    "maxBytes": 1024 * 1024,  # 1Mb
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                    "filters": ["correlation_id"]
                 }
             },
             "loggers": {
-                "facepalm": {
+                "uvicorn": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "INFO",
+                    "propagate": False
+                },
+                "databases": {
                     "handlers": ["default"],
+                    "level": "WARN",
+                    "propagate": False
+                },
+                "aiosqlite": {
+                    "handlers": ["default"],
+                    "level": "WARN",
+                    "propagate": False
+                },
+                "facepalm": {
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 }
