c7bc4fbeb455dbb9258740a89316bb2c81d55182
varad <py.varad@gmail.com>
2024-08-23 07:05:33 +0530

    Added support for obfuscation and making logs as JSON

--- facepalm/logging_config.py
+++ facepalm/logging_config.py
@@ -1,6 +1,29 @@
+import logging
 from logging.config import dictConfig
 
 from facepalm.config import DevConfig, config
+
+
+def obfuscated(email: str, obfuscated_length: int) -> str:
+    characters = email[:obfuscated_length]
+    first, last = email.split("@")
+    return characters + ("*" * (len(first) - obfuscated_length)) + "@" + last
+
+
+handlers = ["default", "rotating_file"]
+if isinstance(config, DevConfig):
+    handlers = ["default", "rotating_file"]
+
+
+class EmailObfuscationFilter(logging.Filter):
+    def __init__(self, name: str = "", obfuscated_length: int = 2) -> None:
+        super().__init__(name)
+        self.obfuscated_length = obfuscated_length
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        if "email" in record.__dict__:
+            record.email = obfuscated(record.email, self.obfuscated_length)
+        return True
 
 
 def configure_logging() -> None:
@@ -13,6 +36,10 @@ def configure_logging() -> None:
                     "()": "asgi_correlation_id.CorrelationIdFilter",
                     "uuid_length": 8 if isinstance(config, DevConfig) else 32,
                     "default_value": "-"
+                },
+                "email_obfuscation": {
+                    "()": EmailObfuscationFilter,
+                    "obfuscated_length": 2 if isinstance(config, DevConfig) else 0
                 }
             },
             "formatters": {
@@ -22,10 +49,10 @@ def configure_logging() -> None:
                     "format": "(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s",
                 },
                 "file": {
-                    "class": "logging.Formatter",
+                    "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
                     "datefmt": "%y-%m-%dT%H:%M:%S",
-                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | [%(correlation_id)s] %(name)s:%(lineno)d "
-                              "- %(message)s",
+                    "format": "%(asctime)s %(msecs)03dZ %(levelname)-8s [%(correlation_id)s] %(name)s:%(lineno)d "
+                              " %(message)s",
                 }
             },
             "handlers": {
@@ -33,7 +60,7 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                    "filters": ["correlation_id"]
+                    "filters": ["correlation_id", "email_obfuscation"]
                 },
                 "rotating_file": {
                     "class": "logging.handlers.RotatingFileHandler",
@@ -43,7 +70,7 @@ def configure_logging() -> None:
                     "maxBytes": 1024 * 1024,  # 1Mb
                     "backupCount": 5,
                     "encoding": "utf8",
-                    "filters": ["correlation_id"]
+                    "filters": ["correlation_id", "email_obfuscation"]
                 }
             },
             "loggers": {
@@ -63,7 +90,7 @@ def configure_logging() -> None:
                     "propagate": False
                 },
                 "facepalm": {
-                    "handlers": ["default", "rotating_file"],
+                    "handlers": handlers,
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 }
